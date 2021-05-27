8ee25dccb9513cfb0eb3664058b3d15cadb15b10
AlexDavicenko <adavicenko@gmail.com>
2021-05-27 20:52:47 +0100

    1.0

--- static/js/main.15f060dd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{10:function(e,n,t){},11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),s=t(3),i=t.n(s),a=(t(8),t(0)),j=function(){return Object(a.jsx)("header",{children:Object(a.jsx)("h1",{children:"Tracker"})})};var o=function(){return Object(a.jsx)("div",{className:"App",children:Object(a.jsx)(j,{})})};t(10);i.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(o,{})}),document.getElementById("root"))},8:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.15f060dd.chunk.js.map
